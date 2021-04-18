0efbcf4a02427e7b6890a02e036f755b8c8376e9
张岩 <22051197@zju.edu.cn>
2021-04-18 17:15:59 +0800

    pro01

---
+++ day05/ForForExer1.java
@@ -0,0 +1,15 @@
+class ForForExer1{
+	public static void main(String[] args){
+		for(int i = 1; i < 10; i++){
+			for(int j = 1; j <= i; j++){
+				System.out.print(j + "*" + i + "=" + (i*j));
+				if(i != j){
+					System.out.print(" ");
+				}else{
+
+					System.out.println();
+				}
+			}
+		}
+	}
+}
