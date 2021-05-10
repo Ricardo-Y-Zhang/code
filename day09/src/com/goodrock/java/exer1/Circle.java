6593814fe5181ba63ef853f7f4df342e3b9d8a05
张岩 <22051197@zju.edu.cn>
2021-05-10 23:46:58 +0800

    day09

---
+++ day09/src/com/goodrock/java/exer1/Circle.java
@@ -0,0 +1,11 @@
+package src.com.goodrock.java.exer1;
+
+public class Circle {
+    double radius;
+
+    public double Area(){
+        return Math.PI * radius * radius;
+    }
+}
+
+
---
+++ day09/src/com/goodrock/java/exer1/PassObject.java
@@ -0,0 +1,22 @@
+package src.com.goodrock.java.exer1;
+
+public class PassObject {
+    public static void main(String[] args){
+        Circle circle = new Circle();
+
+        PassObject test = new PassObject();
+
+        test.printAreas(circle, 5);
+
+        System.out.println("now radius is " + circle.radius);
+    }
+    public void printAreas(Circle c, int time){
+        System.out.println("Radius\t\tArea");
+
+        for(int i = 1; i <= time; i++){
+            c.radius = i;
+            System.out.println(c.radius + "\t\t" + c.Area());
+        }
+        c.radius = time + 1;
+    }
+}
