51b16592ad71f3426a04fded1ef6541c2a3d212f
Ricardo-Y-Zhang <22051197@zju.edu.cn>
2021-06-14 17:02:41 +0800

    setting

--- day18/src/cn/zy/java/exer1/HelloWorld.java
+++ day18/src/cn/zy/java/exer1/HelloWorld.java
@@ -5,3 +5,30 @@ public class HelloWorld {
         System.out.println("hello, world!");
     }
 }
+
+
+/**
+ * @program: ${PROJECT_NAME}
+ *
+ * @description: ${description}
+ *
+ * @author: Mr.Yan
+ *
+ * @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ **/
+
+
+
+**
+        * description: $description$
+        $params$
+        * @return  $returns$
+        * @author: Mr.Yan
+        * @date: $date$
+        **/
+
+
+
+
+
+        groovyScript("def result=''; def params=\"${_1}\".replaceAll('[\\\\[|\\\\]|\\\\s]', '').split(',').toList(); for(i = 0; i < params.size(); i++) {result+=' * @param  ' + params[i] + ((i < params.size() - 1) ? '\\n' : '')}; return result", methodParameters())
