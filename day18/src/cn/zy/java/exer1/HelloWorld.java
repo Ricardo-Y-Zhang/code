acda3c9e4edaeb2e2f60297fc94ce61c81132017
张岩 <22051197@zju.edu.cn>
2021-06-14 17:12:49 +0800

    settings

--- day18/src/cn/zy/java/exer1/HelloWorld.java
+++ day18/src/cn/zy/java/exer1/HelloWorld.java
@@ -1,34 +1,19 @@
 package cn.zy.java.exer1;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("hello, world!");
     }
 }
 
 
-/**
- * @program: ${PROJECT_NAME}
- *
- * @description: ${description}
- *
- * @author: Mr.Yan
- *
- * @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
- **/
-
-
-
-**
-        * description: $description$
-        $params$
-        * @return  $returns$
-        * @author: Mr.Yan
-        * @date: $date$
-        **/
 
 
 
 
 
-        groovyScript("def result=''; def params=\"${_1}\".replaceAll('[\\\\[|\\\\]|\\\\s]', '').split(',').toList(); for(i = 0; i < params.size(); i++) {result+=' * @param  ' + params[i] + ((i < params.size() - 1) ? '\\n' : '')}; return result", methodParameters())
+
+
+
+
