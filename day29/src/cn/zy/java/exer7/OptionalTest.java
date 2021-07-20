37f6736938cda8c393a2c0820d74c4dcb5069186
Ricardo-Y-Zhang <22051197@zju.edu.cn>
2021-07-20 19:28:08 +0800

    day30

--- day29/src/cn/zy/java/exer7/OptionalTest.java
+++ day29/src/cn/zy/java/exer7/OptionalTest.java
@@ -37,5 +37,27 @@ public class OptionalTest {
 
     }
 
+    @Test
+    public void test3(){
+        //emtpy(): 创建空的Optional对象，内部value为null
+        Optional<Object> empty = Optional.empty();
+        //判断是否包含对象
+        System.out.println(empty.isPresent());//false
+
+        //of(T t)：封装数据t生成Optional对象，t必须为非空
+        Optional<String> optionalS = Optional.of("hello");
+        //如果Optional封装的数据为空，则get()报错
+        System.out.println(optionalS.get());//hello
+    }
+
+    @Test
+    public void test4(){
+
+        //ofNullable(T t)：不要求t非空
+        Optional<String> str = Optional.ofNullable("str");
+        //orElse(T t1)：如果Optional内部的value非空，则返回此value值。若value为空，返回t1
+        String str2 = str.orElse("str2");
+
+    }
 
 }
